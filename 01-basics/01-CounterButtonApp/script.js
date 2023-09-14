72fad7a69b73028ef63c21f5c82d750b641add1e
kulabuha <kulabuha@gmail.com>
2023-09-14 22:03:27 +0300

    Solve CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,16 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+    template :'<button @click="handleClick" type="button">{{num}}</button>',
+    data() {
+        return {
+            num : 0,
+        }
+    },
+    methods: {
+        handleClick() {
+            this.num++;
+        }
+    }    
+
+}).mount('#app');

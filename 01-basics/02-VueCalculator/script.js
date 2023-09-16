e1a510a98f1063dca9aeb9575113db76e6a54b57
kulabuha <kulabuha@gmail.com>
2023-09-16 22:39:28 +0300

    Solve VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+
+const RootComponent = {
+    template: `<div class="row">
+                    <div class="col">
+                        <input type="number" v-model='firstNumber' />
+                    </div>
+
+                    <div class="col" style="display: flex; flex-direction: column; font-family: emoji">
+                        <label><input type="radio" v-model='operator' name="operator" value="sum" /> ➕</label>
+                        <label><input type="radio" v-model='operator' name="operator" value="subtract" /> ➖</label>
+                        <label><input type="radio" v-model='operator' name="operator" value="multiply" /> ✖</label>
+                        <label><input type="radio" v-model='operator' name="operator" value="divide" /> ➗</label>
+                    </div>
+
+                    <div class="col">
+                        <input type="number" v-model="secondNumber" />
+                    </div>
+
+                    <div class="col">=</div>
+
+                    <output class="col">{{ result }}</output>
+                </div>`,
+    data() { 
+        return {
+            operator: "sum",
+            operatorsMap: {
+                sum:        "+",
+                subtract:   "-",
+                multiply:   "*",
+                divide:     "/",
+            },
+            firstNumber: 0,
+            secondNumber: 0,
+        }
+    },
+    computed: {
+        result() {return eval(this.firstNumber + this.operatorsMap[this.operator] + this.secondNumber)},
+    },
+}
+createApp(RootComponent).mount('#app');

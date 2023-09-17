523372cfd41d86af54427767ab0844b6d1347ce4
kulabuha <kulabuha@gmail.com>
2023-09-17 14:14:37 +0300

    Solve MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,33 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const RootComponent = {
+  template:       `<div>
+                    <label><input type="radio" v-model="titleId" name="meetupId" value="1" /> 1</label>
+                    <label><input type="radio" v-model="titleId" name="meetupId" value="2" /> 2</label>
+                    <label><input type="radio" v-model="titleId" name="meetupId" value="3" /> 3</label>
+                    <label><input type="radio" v-model="titleId" name="meetupId" value="4" /> 4</label>
+                    <label><input type="radio" v-model="titleId" name="meetupId" value="5" /> 5</label>
+                    <hr />
+                    <h3>{{meetupTitle}}</h3>
+                  </div>`,
+  data(){
+    return{
+        meetupTitle: "",
+        titleId: ""
+    }
+  },
+  watch: {
+    titleId(){
+      fetchMeetupById(this.titleId)
+      .then((json) => {
+        this.meetupTitle = json.title;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });}
+  },
+}
+
+
+createApp(RootComponent).mount('#app')
